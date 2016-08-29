140e17a803af3d0f63f74ffb86d9d910644f8011
thamera <architect@hamera.com>
2016-08-28 20:51:20 -0700

    Update

---
+++ scripts/powerup.js
@@ -0,0 +1,25 @@
+﻿var boardButtonCallback = function (t, options) {
+    return t.overlay({
+        url: './index.html',
+        args: {test: "test"}
+    });
+};
+
+TrelloPowerUp.initialize({
+    'board-buttons': function (t, board) {
+        return [
+            {
+                icon: './images/pci38.png',
+                text: 'Implementation Apps',
+                callback: boardButtonCallback
+            }
+        ];
+    },
+    'show-settings': function (t, options) {
+        return t.popup({
+            title: 'Settings',
+            url: './settings.html',
+            height: 184
+        });
+    }
+});
